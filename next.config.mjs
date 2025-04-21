fc73cdfa63568dfd1396eee9bba3b4582cb81ba0
mr-singh96-ux <khalsakiratbirsingh@gmail.com>
2025-04-21 21:52:48 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['cdn-icons-png.flaticon.com'],
-    unoptimized: true, // Optional: disables Next's image optimization for external hosts
-  },
-  // Do NOT include `output: 'export'` if using Clerk (SSR required)
+    images:{
+        domains:['cdn-icons-png.flaticon.com']
+    }
 };
 
 export default nextConfig;
